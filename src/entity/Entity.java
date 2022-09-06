31805bc70daaed49a7915103def3941a9e5fc13f
Diep <hyusieunhan@gmail.com>
2022-09-07 00:36:58 +0700

    Character sprite update

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -11,4 +11,6 @@ public class Entity {
 	//BufferedImage describes an image with an accessible buffer of image data
 	public BufferedImage up1,up2,down1,down2,left1,left2,right1,right2;
 	public String direction;
-}
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
+} 
